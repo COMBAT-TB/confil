044a88baf658a7f21789ed642cb810db81b82200
thobalose <lose.thoba@gmail.com>
2019-02-15 15:16:39 +0200

    versioning.

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -4,11 +4,11 @@ PKG_NAME=confil
 USER=thoba
 
 OS=$TRAVIS_OS_NAME-64
-mkdir ~/conda-bld
-wget https://github.com/COMBAT-TB/confil/archive/0.0.1.tar.gz
-export SHA=$(sha256sum 0.0.1.tar.gz | awk '{print $1}')
+
 conda config --set anaconda_upload no
-export VERSION=`date +%Y.%m.%d`
+DEV='dev'
+DATE=`date +%Y%m%d`
+export VERSION=$DEV$DATE
 export CONDA_BLD_PATH=$HOME/miniconda/conda-bld
 conda build .
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
