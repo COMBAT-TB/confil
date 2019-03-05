4ccf35ebf446712d01fb15c401990670789df708
thobalose <lose.thoba@gmail.com>
2019-03-05 10:40:43 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='confil',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/COMBAT-TB/confil',
     description='Contamination filter',
     long_description=long_description,
